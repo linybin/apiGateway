3cb9328dd35dd1ba31cb7806cae3c3b0f4491fa6
Ben Lin <linybin@gmail.com>
2017-01-10 16:51:54 +0800

    update dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,22 @@ var minify = require("gulp-minify");
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 
-var src = ["src/lib/**/*.js", "src/apigClient.js"];
+var srcs= [
+    "src/lib/axios/dist/axios.standalone.js",
+    "src/lib/CryptoJS/rollups/hmac-sha256.js",
+    "src/lib/CryptoJS/rollups/sha256.js",
+    "src/lib/CryptoJS/components/hmac.js",
+    "src/lib/CryptoJS/components/enc-base64.js",
+    "src/lib/url-template/url-template.js",
+    "src/lib/apiGatewayCore/sigV4Client.js",
+    "src/lib/apiGatewayCore/apiGatewayClient.js",
+    "src/lib/apiGatewayCore/simpleHttpClient.js",
+    "src/lib/apiGatewayCore/utils.js",
+    "src/apigClient.js"]
+
 
 gulp.task("default", function(){
-    gulp.src(src)
+    gulp.src(srcs)
         .pipe(concat("api-gateway-sdk.js"))
     .pipe(minify())
     .pipe(gulp.dest("dist/"));
